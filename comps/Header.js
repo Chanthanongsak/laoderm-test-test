3b1336c5851f00e1937bde7e2f012bf89329d881
Chanthanongsak <jouykpk@gmail.com>
2022-09-27 14:53:57 +0700

    deploy

--- comps/Header.js
+++ comps/Header.js
@@ -216,44 +216,6 @@ function Header() {
     })();
   }, []);
 
-  // useEffect(() => {
-  //   window.setInterval(function () {
-  //     plusSlides(1);
-  //   }, 5000);
-
-  //   var slideIndex = 1;
-  //   showSlides(slideIndex);
-
-  //   function plusSlides(n) {
-  //     showSlides((slideIndex += n));
-  //   }
-
-  //   function currentSlide(n) {
-  //     showSlides((slideIndex = n));
-  //   }
-
-  //   function showSlides(n) {
-  //     var i;
-  //     var slides = document.getElementsByClassName("mySlides");
-  //     if (n > slides.length) {
-  //       slideIndex = 1;
-  //     }
-  //     if (n < 1) {
-  //       slideIndex = slides.length;
-  //     }
-  //     for (i = 0; i < slides.length; i++) {
-  //       slides[i].style.display = "none";
-  //       slides[slideIndex - 1].style.display = "block";
-  //     }
-
-  //     // if (window.location.pathname !== "/") {
-  //     //   if (slides[slideIndex - 1] === "undefined") {
-  //     //     return;
-  //     //   }
-  //     // }
-  //   }
-  // }, []);
-
   if (title === "title1_1") {
   }
 
