dcda31b817741e0b5879144af828f8c7e4a6c135
Chanthanongsak <jouykpk@gmail.com>
2022-10-12 14:31:41 +0700

    deploy

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
 import Layout from "../comps/Layout";
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+        />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
